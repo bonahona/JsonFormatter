1b336eef9e8b8f7c039b679e0cc2a674ec825902
Bona <bona.fyrvall@arrowheadgs.com>
2020-08-21 13:27:12 +0200

    Initial commit.

---
+++ JsonFormatter/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace JsonFormatter {
+    partial class JsonFormatter {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JsonFormatter));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtJsonPathOutput = new System.Windows.Forms.TextBox();
+            this.txtJsonPathExpression = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtJsonPathOutput);
+            this.splitContainer1.Panel1.Controls.Add(this.txtJsonPathExpression);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtInput);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtOutput);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(933, 551);
+            this.splitContainer1.SplitterDistance = 325;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtJsonPathOutput
+            // 
+            this.txtJsonPathOutput.Location = new System.Drawing.Point(715, 49);
+            this.txtJsonPathOutput.Multiline = true;
+            this.txtJsonPathOutput.Name = "txtJsonPathOutput";
+            this.txtJsonPathOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJsonPathOutput.Size = new System.Drawing.Size(216, 273);
+            this.txtJsonPathOutput.TabIndex = 4;
+            // 
+            // txtJsonPathExpression
+            // 
+            this.txtJsonPathExpression.Location = new System.Drawing.Point(715, 22);
+            this.txtJsonPathExpression.Name = "txtJsonPathExpression";
+            this.txtJsonPathExpression.Size = new System.Drawing.Size(216, 20);
+            this.txtJsonPathExpression.TabIndex = 3;
+            this.txtJsonPathExpression.TextChanged += new System.EventHandler(this.txtJsonPathExpression_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(715, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "JSON Path";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(3, 3);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(705, 319);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(0, 3);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(708, 216);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.Location = new System.Drawing.Point(645, 573);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Prettify";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // JsonFormatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 608);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JsonFormatter";
+            this.Text = "Json Formatter";
+            this.Load += new System.EventHandler(this.JsonFormatter_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtJsonPathOutput;
+        private System.Windows.Forms.TextBox txtJsonPathExpression;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JsonFormatter/Form1.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonFormatter {
+    public partial class JsonFormatter : Form {
+        public JsonFormatter() {
+            InitializeComponent();
+        }
+
+        private void JsonFormatter_Load(object sender, EventArgs e) {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e) {
+
+            txtOutput.Clear();
+
+            try {
+                var jsonObject = JsonConvert.DeserializeObject(txtInput.Text);
+                var outputText = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+                txtOutput.Text = outputText;
+            } catch (Exception ex) {
+                txtOutput.Text = ex.Message;
+            }
+        }
+
+        private void txtJsonPathExpression_TextChanged(object sender, EventArgs e) {
+            try {
+                if (string.IsNullOrEmpty(txtOutput.Text)) {
+                    return;
+                }
+
+                txtJsonPathOutput.Clear();
+
+                var jsonObject = JToken.Parse(txtInput.Text);
+                var selectedTokens = jsonObject.SelectTokens(txtJsonPathExpression.Text);
+
+                txtJsonPathOutput.Text = string.Join(Environment.NewLine, selectedTokens.Select(t => t.ToString()));
+            } catch (Exception ex) {
+                txtJsonPathOutput.Text = ex.Message;
+            }
+        }
+    }
+}
